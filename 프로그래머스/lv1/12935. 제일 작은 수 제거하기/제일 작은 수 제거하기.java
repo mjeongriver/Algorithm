ad2217d2d2b41e900fca6f75a8bbe2dd36fab0a2
Kang Minjeong <116552282+kangminjeong8@users.noreply.github.com>
2023-06-09 18:00:47 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.73 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,37 +1,29 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] arr) {
 
-        //배열 길이가 1인 경우
-        if(arr.length == 1){
-            int[] answer = {-1};
-            return answer;
-        }
-        
-        //배열길이가 1보다 클 때
-        //가장 작은 수를 뺀 길이로 초기화
-        int[] answer = new int[arr.length-1];
-        
-        //하나의 값을 기준으로 잡음.
-        int min = arr[0]; 
-        
-        //제일 작은 수를 구한다.
-        for(int i=1; i<arr.length; i++){
-            min = Math.min(min, arr[i]);
-        }
-        
-        //반복문 index 용
-        int index = 0;
-        
-        //가장 작은 수를 제외한 배열 만들기
-        for(int i=0; i<arr.length; i++){
-            //배열의 값이 가장 작은 수라면
-            if(arr[i] == min){
-                //continue를 이용해 배열에 넣지 않고 넘어간다.
-                continue;
+        if (arr.length == 1) {
+            arr[0] = -1;
+            return arr;
+        } else {
+            ArrayList < Integer > arrayList = new ArrayList < Integer > ();
+
+            for (int a: arr) {
+                arrayList.add(a);
             }
-            answer[index++] = arr[i];
+
+            Integer minimum = Collections.min(arrayList);
+            arrayList.remove(minimum);
+
+            int[] resultArray = new int[arr.length - 1];
+
+            for (int i = 0; i < arrayList.size(); ++i) {
+                resultArray[i] = arrayList.get(i);
+            }
+            
+            return resultArray;
         }
-        
-        return answer;
     }
+
 }
